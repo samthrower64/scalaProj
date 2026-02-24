936c467bc05dd092bfff8d238b87ee94ecf66e42
samthrower64 <samuelthrower5@gmail.com>
2026-02-24 20:42:19 +1300

    CUST-1: Move customer name generator to own class.

---
+++ src/main/scala/CustomerNameList.scala
@@ -0,0 +1,36 @@
+class CustomerNameList {
+  
+  val firstNames: List[String] = 
+    List(
+      "Alan", 
+      "Sarah", 
+      "Barry", 
+      "Steve", 
+      "Joe", 
+      "Lara", 
+      "Jane", 
+      "Hazel", 
+      "Clammy", 
+      "Lucy", 
+      "Rosa", 
+      "Rob", 
+      "Sam", 
+      "Josh",
+    )
+  
+  val surNames: List[String] = 
+    List(
+      "Corbishley", 
+      "Palin", 
+      "Chuckle", 
+      "Wozniak", 
+      "Bloggs", 
+      "Smith", 
+      "Doe",
+      "McCrory",
+      "Entwistle",
+      "Barnes",
+      "Stryker",
+      "Fording",
+    )
+}
